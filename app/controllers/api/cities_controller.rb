baeacd3e03c3424652687140229be34dfb148414
Hiro Obara <hobara1211@gmail.com>
2017-07-22 18:06:16 -0700

    updated city routes

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -1,13 +1,11 @@
-class Api::CityController < ApplicationController
+class Api::CitiesController < ApplicationController
 
   def show
     @city = City.find(params[:id])
-    render '/api/cities/show'
   end
 
   def index
     @cities = City.all
-    render '/api/cities/index'
   end
 
   def city_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource  :session, only: [:create, :destroy, :show]
-    resources :city, only: [:show, :index]
+    resources :cities, only: [:show, :index]
   end
 
   root to: "static_pages#root"
